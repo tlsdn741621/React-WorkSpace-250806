b0c6245b6357ecbf30289c44ccd605e68ffe81fc
tlsdn741621 <tlsdn741621@gmail.com>
2025-08-07 16:53:41 +0900

    250807 1653

---
+++ ch03-vite-test/src/component/Counter.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    // state의 초깃값 설정
+    this.state = {
+      number: 0,
+      fixedNumber: 0,
+    };
+  }
+  render() {
+    const { number, fixedNumber } = this.state;
+    return (
+      <>
+        <div>
+          <h1>{number}</h1>
+          <h2>고정 값 : {fixedNumber}</h2>
+        </div>
+        <button
+          onClick={() => {
+            // 기존,
+            // this.setState({ number: number + 1 });
+            // this.setState({ number: this.state.number + 1 });
+            // 변경
+            // this.setState(콜백함수1, 콜백함수2)
+            this.setState(
+              (prevState) => {
+                return {
+                  number: prevState.number + 1,
+                };
+              }, // 콜백함수1
+              () => {
+                console.log(
+                  "setState 가 동작 후, 추가 동작하는 함수 정의 해보기. "
+                );
+              } // 콜백함수2
+            );
+            // 한번 더 작성하기.
+            this.setState((prevState) => ({
+              number: prevState.number + 1,
+            }));
+          }}
+        >
+          +1
+        </button>
+        <button
+          onClick={() => {
+            // 기존
+            // this.setState({ number: number - 1 });
+            // 변경
+            // 한번 더 작성하기.
+            this.setState((prevState) => ({
+              number: prevState.number - 1,
+            }));
+            // 한번 더 작성하기.
+            this.setState((prevState) => ({
+              number: prevState.number - 1,
+            }));
+          }}
+        >
+          -1
+        </button>
+      </>
+    );
+  }
+}
+
+export default Counter;
---
+++ ch04-vite-test/src/component/EventPractice.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+
+class EventPractice extends Component {
+  // 초깃값
+  state = {
+    username: "",
+    message: "",
+  };
+
+  // 태그에 직접 정의한 이벤트 핸들링 작업, 따로 분리 작업.
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  // 이벤트 핸들링 따로 정의,
+  handleChange(e) {
+    this.setState({
+      // 변경전
+      //   message: e.target.value,
+      // 변경 후,
+      //input, 2개가 될 예정, 각 input 이름에 해당하는 객체를 불러와서 사용하기.
+      [e.target.name]: e.target.value,
+    });
+  }
+  handleClick() {
+    alert(this.state.username + ":" + this.state.message);
+    this.setState({
+      username: "",
+      message: "",
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>이벤트 연습</h1>
+        <h2>1 onChange 연습 해보기. </h2>
+        <h3>message : {this.state.message}</h3>
+        <h3>username : {this.state.username}</h3>
+        <input
+          type="text"
+          name="message"
+          placeholder="아무거나 입력하세요"
+          value={this.state.message} // getter, state 객체 안의 message 조회
+          // 방법1, 직접 설정
+          //   onChange={(e) => {
+          //     // setter, state 의 객체의 message 값을 변경.
+          //     this.setState({ message: e.target.value });
+          //   }}
+          // 방법2, 따로 분리 설정.
+          onChange={this.handleChange}
+        />
+
+        <input
+          type="text"
+          name="username"
+          placeholder="사용자명 입력하기"
+          value={this.state.username} // getter, state 객체 안의 message 조회
+          onChange={this.handleChange}
+        />
+
+        <h2>2 onClick 연습 해보기. </h2>
+        <button
+          // 방법1, 직접 설정
+          //   onClick={() => {
+          //     alert(this.state.message);
+          //     this.setState({
+          //       message: "",
+          //     });
+          //   }}
+          // 방법2, 따로 분리 설정.
+          onClick={this.handleClick}
+        >
+          확인
+        </button>
+      </>
+    );
+  }
+}
+
+export default EventPractice;
